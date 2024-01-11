f745cda5756aced48f2d311327c80bab871f95eb
Ezekiel Abodesegun <eabodesegun@appzonegroup.com>
2024-01-11 18:11:10 +0100

    finish queproducer and direct exchange example

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMQ.Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           var factory = new ConnectionFactory
+            {
+               VirtualHost = "IndiaVH",
+                Uri = new Uri("amqp://administrator:admin@localhost:5672")
+            };
+           using var connection = factory.CreateConnection();           
+            using var channel = connection.CreateModel();         
+                    DirectExchangeConsumer.Consume(channel);
+            Console.WriteLine("Consumer started");
+           
+
+            Console.ReadLine();
+           
+        }
+    }
+}
---
+++ RabbitMQ.Producer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace RabbitMQ.Producer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory {
+                VirtualHost= "IndiaVH",
+                Uri = new Uri("amqp://administrator:admin@localhost:5672")
+            };
+           using var connection = factory.CreateConnection();
+           using var channel = connection.CreateModel();
+            DirectExchangeProducer.Publish(channel);
+            Console.WriteLine("Producer Started");
+
+            Console.ReadLine();
+           
+        }
+    }
+}
