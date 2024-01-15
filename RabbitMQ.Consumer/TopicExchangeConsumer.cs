024130a816199155d8cb04fd07885bfe24cf7bf5
Ezekiel Abodesegun <eabodesegun@appzonegroup.com>
2024-01-15 12:27:50 +0100

    started topic exhange

---
+++ RabbitMQ.Consumer/TopicExchangeConsumer.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Consumer
+{
+    public class TopicExchangeConsumer
+    {
+        public static void Consume(IModel channel)
+        {
+
+            channel.ExchangeDeclare("demo-topic-exchange", ExchangeType.Topic);
+            channel.QueueDeclare("demo-direct-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+            channel.QueueBind(queue: "demo-direct-queue", exchange: "demo-direct-exchange", routingKey: "account.init");
+            // declearing a prefetch count
+            // this allows the consumer fetch 10 messages at the same time
+            channel.BasicQos(0, 10, false);
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+            channel.BasicConsume("demo-direct-queue", true, consumer);
+
+        }
+    }
+}
